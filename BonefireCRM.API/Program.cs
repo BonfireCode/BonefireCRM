d8449bf08144b5e0c75003b792a49fde9a768fc0
linkprada <haouaouchi.said@gmail.com>
2025-08-28 16:05:21 -0400

    Add scalar to visualise APIs

--- BonefireCRM.API/Program.cs
+++ BonefireCRM.API/Program.cs
@@ -5,8 +5,11 @@
 using BonefireCRM.Domain;
 using BonefireCRM.Infrastructure;
 using FastEndpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFastEndpoints();
 
 builder.AddDomainDependencies();
 builder.AddInfrastructureDependencies();
@@ -28,31 +31,11 @@ app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseFastEndpoints();
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
