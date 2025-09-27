9295baec5d42b3cce56743e33274d6bf92874a8f
linkprada <haouaouchi.said@gmail.com>
2025-09-27 01:55:52 -0400

    Fix auth scheme BearerAndApplication not having a handler

---
+++ BonefireCRM.Infrastructure/Security/CompositeAuthenticationHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+public class CompositeAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    private readonly IAuthenticationSchemeProvider _schemes;
+
+    public CompositeAuthenticationHandler(
+        IAuthenticationSchemeProvider schemes,
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder) : base(options, logger, encoder)
+    {
+        _schemes = schemes;
+    }
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var schemeNames = new[] 
+        { 
+            IdentityConstants.BearerScheme,
+            IdentityConstants.ApplicationScheme 
+        };
+
+        foreach (var name in schemeNames)
+        {
+            var result = await Context.AuthenticateAsync(name);
+            if (result?.Succeeded == true)
+            {
+                return AuthenticateResult.Success(result.Ticket);
+            }
+        }
+
+        return AuthenticateResult.NoResult();
+    }
+}
