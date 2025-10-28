f83f8555d5c00e320d7df92b313e5dc0e40000e6
linkprada <haouaouchi.said@gmail.com>
2025-10-27 22:21:21 -0400

    Add a source generator for QueryExpressions

---
+++ BonefireCRM.SourceGenerator.Tests/Common/ModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BonefireCRM.SourceGenerator.Tests.Common
+{
+    public static class ModuleInitializer
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            VerifySourceGenerators.Initialize();
+        }
+    }
+}
---
+++ BonefireCRM.SourceGenerator.Tests/FilterGeneratorTests.cs
@@ -0,0 +1,76 @@
+﻿using BonefireCRM.SourceGenerator.Tests.Common;
+
+namespace BonefireCRM.SourceGenerator.Tests
+{
+    public class FilterGeneratorTests
+    {
+        private readonly string _attributeMarker;
+
+        public FilterGeneratorTests()
+        {
+            _attributeMarker = """
+            namespace BonefireCRM.SourceGenerator
+            {
+                [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+                public class ApplyFilteringAttribute : Attribute
+                {
+                    public Type EntityType { get; set; }
+
+                    public ApplyFilteringAttribute(Type entityType)
+                    {
+                        EntityType = entityType;
+                    }
+                }
+            }
+            """;
+        }
+
+        [Fact]
+        public Task GeneratesClassCorrectly()
+        {
+            var generator = new FilterGenerator();
+            var source = $$"""
+                using System;
+                using System.Collections.Generic;
+                using BonefireCRM.SourceGenerator;
+                using BonefireCRM.Targets;
+
+                {{_attributeMarker}}
+
+                namespace BonefireCRM.Definitions
+                {
+                    [ApplyFiltering(typeof(MyEntity))]
+                    public class TestClass
+                    {
+                        public Guid? Id { get; set; }
+                        public string? FirstName { get; set; }
+                        public string? LastName { get; set; }
+                        public DateTime? Age { get; set; }
+                        public int? Count { get; set; }
+
+                        public string SortBy { get; set; }
+                        public string SortDirection { get; set; } = "ASC";                   
+                    }
+                }
+
+                namespace BonefireCRM.Targets
+                {
+                    public class MyEntity
+                    {
+                        public Guid? Id { get; set; }
+                        public string? FirstName { get; set; }
+                        public string? LastName { get; set; }
+                        public DateTime? Age { get; set; }
+                        public int? Count { get; set; }
+                        public int[] Counters { get; set; }
+                        public List<int> Counters2 { get; set; }
+                    }
+                }
+                """;
+
+            var result = TestHelper.Verify(source, generator);
+
+            return Verify(result).UseDirectory("Snapshots");
+        }
+    }
+}
