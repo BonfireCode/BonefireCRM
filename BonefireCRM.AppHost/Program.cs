f2c2945d991594dcd78ab287cef4f658fa941975
linkprada <haouaouchi.said@gmail.com>
2025-12-06 04:52:50 -0500

    Update Aspire and improve config for launching angular project

--- BonefireCRM.AppHost/Program.cs
+++ BonefireCRM.AppHost/Program.cs
@@ -2,9 +2,15 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.BonefireCRM_API>("apiservice");
 
-builder.AddProject<Projects.BonefireCRM_Web_Server>("webfrontend")
+//var webFrontend = builder.AddProject<Projects.BonefireCRM_Web_Server>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService)
+//    .WaitFor(apiService);
+
+var frontend = builder.AddJavaScriptApp("frontend", "../BonefireCRM.Web/bonefirecrm.web.client", runScriptName: "start")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithHttpEndpoint(env: "PORT")
     .WaitFor(apiService);
 
 builder.Build().Run();
---
+++ BonefireCRM.Web/bonefirecrm.web.client/proxy.conf.js
@@ -0,0 +1,29 @@
+const { env } = require('process');
+
+const aspireTarget =
+  env["services__apiservice__https__0"]
+  || env["services__apiservice__http__0"];
+
+const manualTarget = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+    ? env.ASPNETCORE_URLS.split(';')[0]
+    : 'https://localhost:7119';
+
+const target = aspireTarget || manualTarget;
+
+const secure = env.NODE_ENV
+  ? env["NODE_ENV"] !== "development"
+  : false
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/api/**", "/weatherforecast"
+    ],
+    target: target,
+    secure: secure,
+  }
+]
+
+module.exports = PROXY_CONFIG;
